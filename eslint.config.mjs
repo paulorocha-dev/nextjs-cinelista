d9459282964a36a19ed2bd1595b70ee200ac5fd5
Paulo Henrique <paulo@rocha.com>
2026-03-03 18:06:41 -0300

    chore: update eslint to version 9.39.3 and ensure eslint-config-prettier uses caret versioning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,15 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import prettier from "eslint-config-prettier/flat";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "eslint-config-prettier",
-  ),
-];
-
-export default eslintConfig;
+export default defineConfig([
+  ...nextVitals,
+  prettier,
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    "node_modules/**",
+  ]),
+]);
