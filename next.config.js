91d34e59a6a6f9176f24043ea97fe7e01f3f0479
Paulo Henrique <paulo@rocha.com>
2026-03-03 18:18:38 -0300

    refactor: atualizar configuração de imagens no next.config.js e remover next.config.ts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-// next.config.js
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ["image.tmdb.org"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/**",
+      },
+    ],
   },
 };
+
+module.exports = nextConfig;
